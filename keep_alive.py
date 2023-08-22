f450918aba998e192e675f69f03cf27f53d1724b
Jason Liu <jasonliu2468@hotmail.com>
2023-08-22 01:23:27 -0400

    Made the app production ready

--- keep_alive.py
+++ keep_alive.py
@@ -9,9 +9,13 @@ def home():
   return "Hello. I am alive!"
 
 
-def run():
-  app.run(host='0.0.0.0', port=8080)
+# def run():
+#   app.run(host='0.0.0.0', port=8080)
 
+# for production
+def run():
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=8080)
 
 def keep_alive():
   t = Thread(target=run)
